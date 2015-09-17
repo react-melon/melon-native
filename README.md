2a88ba7bdb511147e551c27755b2899b85e9164f
ChenXiao <chenxiao07@baidu.com>
2015-09-17 19:28:43 +0800

    将示例移动到单独的文件夹

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # melon-native
 
 ```shell
-git clone https://github.com/react-melon/melon-native.git melonNative && cd melonNative
+git clone https://github.com/react-melon/melon-native.git
 npm install
+npm start
+```
+
+## 添加示例
+
+```shell
+cd examples
+react-native init example1
+cd example1
+```
+
+修改 `AppDelegate.m` 的
+
+```
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+
+```
+
+为
+
+```
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/examples/example1/index.ios.bundle"];
+
 ```
---
+++ examples/simpleui/index.ios.js
@@ -0,0 +1,40 @@
+/**
+ * @file melon-native Example
+ * @author cxtom(cxtom2008@gmail.com)
+ */
+
+'use strict';
+
+var React = require('react-native');
+var {
+    AppRegistry,
+    StyleSheet,
+    View
+} = React;
+
+var Button = require('../../src/Button');
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    button: {
+        marginVertical: 5
+    }
+});
+
+var simpleui = React.createClass({
+    render: function () {
+        return (
+            <View style={styles.container}>
+                <Button style={styles.button}>一个按钮</Button>
+                <Button style={styles.button} disabled>一个按钮</Button>
+            </View>
+        );
+    }
+});
+
+AppRegistry.registerComponent('simpleui', () => simpleui);
